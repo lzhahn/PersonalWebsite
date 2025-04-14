0ef05d5e307d3532db487db142f531f17f6343ab
Lucas <lazarhahn@gmail.com>
2025-04-13 22:11:29 -0700

    Fix Base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/PersonalWebsite' : '',
+  basePath: '',
   images: {
     unoptimized: true,
   },
